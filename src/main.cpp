4f010f58e1360ddc2bc4af17733af286f89c1857
GurkeX <rk@michi-kathi.de>
2024-02-06 20:25:16 +0100

    led light reciever and sender working

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,32 @@ void setup() {
   // put your setup code here, to run once:
   readLED();
 
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
   pinMode(anode, OUTPUT);
-  digitalWrite(anode, HIGH);
+  pinMode(kathode, OUTPUT);
+  digitalWrite(kathode, LOW);
+  //digitalWrite(anode, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  pinMode(A0, INPUT);
+  digitalWrite(anode, LOW);
+  delay(500);
+  Serial.print(analogRead(A0));
+  delay(500);
+
+  pinMode(A0, OUTPUT);
+  analogWrite(A0, LOW);
+  digitalWrite(anode, HIGH);
+  Serial.print(" led an\n");
+  delay(500);
+  
+
 }
+
+// Function to encode string message recieved via serial into 8bit message, that can be sent using 0s and 1s
 
 // put function definitions here:
 int readLED() {

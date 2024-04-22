d456456e5b11efd1087c1de66092f77a74481c0a
GurkeX <rk@michi-kathi.de>
2024-04-22 18:46:41 +0200

    lower bit duration

--- lib/LedComm/led_comm.h
+++ lib/LedComm/led_comm.h
@@ -5,9 +5,9 @@
 
 #define START_BIT 1                   // Start bit is always  1
 #define STOP_BIT 0                    // Stop bit is always  0
-#define BIT_DURATION 10               // Duration of one bit in milliseconds (cant be lower that LIT_UP_DISCHARGE_TIME)
-#define LIT_UP_DISCHARGE_TIME_MS 6    // Time in milliseconds it takes for the led to discharge when direct light shines on it 
-#define NO_LIGHT_DISCHARGE_TIME_MS 18 // Time in milliseconds it take for the led to discharge when no direct light shines on it
+#define BIT_DURATION 7               // Duration of one bit in milliseconds (cant be lower that LIT_UP_DISCHARGE_TIME)
+#define LIT_UP_DISCHARGE_TIME_MS 4    // Time in milliseconds it takes for the led to discharge when direct light shines on it 
+#define NO_LIGHT_DISCHARGE_TIME_MS 10 // Time in milliseconds it take for the led to discharge when no direct light shines on it
 
 
 void initLedComm(); 
